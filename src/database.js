ba1bef964180313da521aef18a6fad3d4649b3e3
Gonzalolog <gonzalolog@gmail.com>
2023-09-06 15:06:31 -0300

    Made sql connection be a pool

--- src/database.js
+++ src/database.js
@@ -2,11 +2,12 @@ const mysql = require("mysql");
 const config = require("../config.json");
 const EventEmitter = require("events");
 const dbEmitter = new EventEmitter();
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: config.host,
     user: config.user,
     password: config.password,
-    database: config.database
+    database: config.database,
+    connectionLimit : 10,
 });
 
 connection.connect((err) => {
