88c7ea5cb2b32d85ec646832e03b512b459412b0
Wookie <bq1214@gmail.com>
2023-09-09 01:03:06 +0500

    fix: formatting

--- src/database.js
+++ src/database.js
@@ -7,42 +7,48 @@ const connection = mysql.createPool({
     user: config.user,
     password: config.password,
     database: config.database,
-    connectionLimit : 10,
+    connectionLimit: 10,
 });
 
 function process(entry) {
-    entry.name = entry.error.slice(0, 192);
-    entry.stack = entry.stack.replace(/'/gm, "");
+    entry.name = entry.error.trim().slice(0, 192);
+    entry.stack = entry.stack
+        .trim()
+        .replace(/\s/g, "")
+        .replace(/'/g, "")
+        .replace(/\\n/g, "<br />");
+
     return entry;
 }
 
-connection.query(`CREATE TABLE IF NOT EXISTS errors (
+connection.query(
+    `CREATE TABLE IF NOT EXISTS errors (
     id INT NOT NULL AUTO_INCREMENT,
     hash VARCHAR(255) NOT NULL,
     error TEXT NOT NULL,
     stack TEXT NOT NULL,
     realm VARCHAR(8) NOT NULL,
-    PRIMARY KEY (id));`, function(err){
+    PRIMARY KEY (id));`,
+    (err) => {
         console.log("Connected to database, downloading cache");
         let cache = {};
-        connection.query("SELECT * FROM errors", (err, rows, fields) => {
+        connection.query("SELECT * FROM errors", (err, rows) => {
             if (err) {
                 console.log("Error downloading cache\n" + err);
                 return;
             }
-    
-            for (var i = 0; i < rows.length; i++) {
-                cache[rows[i].hash] = process(rows[i]);
+
+            for (const row of rows) {
+                cache[row.hash] = process(row);
             }
-    
+
             dbEmitter.emit("initialized", cache);
         });
-
     }
 );
 
 module.exports = {
-    conn : connection,
-    event : dbEmitter,
-    process : process
+    conn: connection,
+    event: dbEmitter,
+    process: process,
 };
